6324b6aeccb80563b4c1fb3056bad67dfe741256
Jonathan Crescentini <jonathan.crescentini@studio.unibo.it>
2026-08-26 17:30:35 +0200

    feat: added viewmodel and factory to GameScreen

--- uicompose/src/main/java/com/unibo/mobile/uicompose/screens/GameScreen.kt
+++ uicompose/src/main/java/com/unibo/mobile/uicompose/screens/GameScreen.kt
@@ -7,20 +7,31 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.unibo.mobile.domain.di.UseCaseProvider
 import com.unibo.mobile.uicompose.components.gamescreen.GameView
 import com.unibo.mobile.uicompose.components.gamescreen.PlayerControls
+import com.unibo.mobile.uicompose.viewmodel.GameScreenViewModel
+import com.unibo.mobile.uicompose.viewmodel.GameScreenViewModelFactory
 
 
 @Composable
 fun GameScreen(
     modifier: Modifier = Modifier,
     onNavigateToMenu: () -> Unit,
-    onNavigateToEndScreen: () -> Unit //TODO: Aggiungere boolean o simili per win/loss verso EndScreen
-    //viewModel: GameScreenViewModel = viewModel(
-    //    factory = GameScreenViewModelFactory(
-    //        [...]UseCase = UseCaseProvider.[...]UseCase
-    //    )
-    //) TODO: SOSTITUIRE CON VIEWMODEL E FACTORY
+    onNavigateToEndScreen: () -> Unit, //TODO: Aggiungere boolean o simili per win/loss verso EndScreen
+    viewModel: GameScreenViewModel = viewModel(
+        factory = GameScreenViewModelFactory(
+            calculateAbilityResultUseCase = UseCaseProvider.calculateAbilityResultUseCase,
+            fetchEnemyByChallengeRatingUseCase = UseCaseProvider.fetchEnemyByChallengeRatingUseCase,
+            fetchAbilityByNameUseCase = UseCaseProvider.fetchAbilityByNameUseCase,
+            determineGamePhaseUseCase = UseCaseProvider.determineGamePhaseUseCase,
+            determineChallengeRatingUseCase = UseCaseProvider.determineChallengeRatingUseCase,
+            applyPlayerAbilityCostUseCase = UseCaseProvider.applyPlayerAbilityCostUseCase,
+            checkCombatStatusUseCase = UseCaseProvider.checkCombatStatusUseCase,
+            applyAbilityResultUseCase = UseCaseProvider.applyAbilityResultUseCase
+        )
+    )
 ) {
     println("Accessing GameScreen")
     /*
@@ -37,8 +48,12 @@ fun GameScreen(
         modifier = modifier.fillMaxSize()
     ) {
         //TODO Aggiungere Logica per decidere il contenuto Combat/Safe
-        GameView(modifier = Modifier.fillMaxHeight(0.5f).fillMaxWidth())
-        PlayerControls(modifier = Modifier.fillMaxHeight(0.5f).fillMaxWidth())
+        GameView(modifier = Modifier
+            .fillMaxHeight(0.5f)
+            .fillMaxWidth())
+        PlayerControls(modifier = Modifier
+            .fillMaxHeight(0.5f)
+            .fillMaxWidth())
     }
 }
 
